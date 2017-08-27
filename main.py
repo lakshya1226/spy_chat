469cc1401e8c9ce1db4f81434842c30ead4989f3
lakshya <1412lucky@gmail.com>
2017-08-27 12:07:32 +0530

    basic conditional statement..

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 print 'Let\'s get started...'
 spy_name = raw_input('what is your name ? : ')
-spy_salutation =raw_input('what should we call you ? :')
-spy_name =spy_salutation +' '+spy_name
-print 'welcome ' + spy_name +' ' +'glad to have you back with us..'
 
+if len(spy_name) > 0 :
+    #LOGIC WILL BE HERE IF CONDITION IS TRUE.
+    spy_salutation =raw_input('what should we call you ? :')
+    spy_name =spy_salutation +' '+spy_name
+    print 'welcome ' + spy_name + ' glad to have you back with us..'
 
