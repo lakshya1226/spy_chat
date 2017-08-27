9f8665deef438eb0154d975b746249db6f834764
lakshya <1412lucky@gmail.com>
2017-08-27 10:04:37 +0530

    adding spy salutation

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ spy_name = raw_input('what is your name ? : ')
 spy_salutation =raw_input('what should we call you ? :')
 spy_name =spy_salutation +' '+spy_name
 print 'welcome ' + spy_name +' ' +'glad to have you back with us..'
-print "test"
+
 
